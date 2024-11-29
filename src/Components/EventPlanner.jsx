509c61c6237363abaca14becc6851d25a2eb4899
bpnva <bp4nvacom@gmail.com>
2024-11-29 17:12:12 -0500

    Added Footer component.

--- src/Components/EventPlanner.jsx
+++ src/Components/EventPlanner.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer'
 import './EventPlanner.css'; // Import CSS file for styling
 
 const EventPlanner = () => {
@@ -75,8 +76,8 @@ const EventPlanner = () => {
                     <button className="submit-button">Send</button>
                 </form>
             </section>            
+            <Footer/>
         </div>
     );
 };
-
 export default EventPlanner;
